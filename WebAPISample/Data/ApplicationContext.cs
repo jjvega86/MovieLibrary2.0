7376c9ff223beb1b8dba4b95f425f3cbee0fa030
jjvega86 <jj.vega86@gmail.com>
2020-11-13 12:41:38 -0500

    Seeded initial data to database, migration added and updated successfully

--- WebAPISample/Data/ApplicationContext.cs
+++ WebAPISample/Data/ApplicationContext.cs
@@ -14,8 +14,10 @@ namespace WebAPISample.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            // Seed data - needs migration
-           
+            modelBuilder.Entity<Movie>().HasData(new Movie { MovieId = 1,  Title = "The Departed", Director = "Martin Scorcese" });
+            modelBuilder.Entity<Movie>().HasData(new Movie { MovieId = 2, Title = "Star Wars", Director = "George Lucas" });
+            modelBuilder.Entity<Movie>().HasData(new Movie { MovieId = 3, Title = "Fellowship of the Ring", Director = "Peter Jackson" });
+
         }
 
         public DbSet<Movie> Movies { get; set; }
---
+++ WebAPISample/Migrations/20201113174031_SeedInitialData.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPISample.Migrations
+{
+    public partial class SeedInitialData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "MovieId", "Director", "Title" },
+                values: new object[] { 1, "Martin Scorcese", "The Departed" });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "MovieId", "Director", "Title" },
+                values: new object[] { 2, "George Lucas", "Star Wars" });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "MovieId", "Director", "Title" },
+                values: new object[] { 3, "Peter Jackson", "Fellowship of the Ring" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Movies",
+                keyColumn: "MovieId",
+                keyValue: 3);
+        }
+    }
+}
