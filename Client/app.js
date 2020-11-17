b23146f9a73657b6b103732c9cb69f084cc5445e
jjvega86 <jj.vega86@gmail.com>
2020-11-17 15:24:52 -0500

    Added GET jQuery function to app.js

--- Client/app.js
+++ Client/app.js
@@ -1,27 +1,57 @@
-(function($){
-    function processForm( e ){
-        var dict = {
-        	Title : this["title"].value,
-            Director: this["director"].value,
-            Genre: this["genre"].value
-        };
+$(document).ready(function(){
+   
+    (function($){
+        function processForm( e ){
+            var dict = {
+                Title : this["title"].value,
+                Director: this["director"].value,
+                Genre: this["genre"].value
+            };
+    
+            $.ajax({
+                url: 'https://localhost:44325/api/movie',
+                dataType: 'json',
+                type: 'post',
+                contentType: 'application/json',
+                data: JSON.stringify(dict),
+                success: function( data, textStatus, jQxhr ){
+                    $('#response pre').html( data );
+                },
+                error: function( jqXhr, textStatus, errorThrown ){
+                    console.log( errorThrown );
+                }
+            });
+    
+            e.preventDefault();
+        }
+    
+        $('#my-form').submit( processForm );
 
-        $.ajax({
+        function getMovies( e ){
+            $.ajax({
             url: 'https://localhost:44325/api/movie',
+            type: 'GET',
             dataType: 'json',
-            type: 'post',
-            contentType: 'application/json',
-            data: JSON.stringify(dict),
-            success: function( data, textStatus, jQxhr ){
-                $('#response pre').html( data );
+            success: function(movies){
+                movieListSuccess(movies);
             },
-            error: function( jqXhr, textStatus, errorThrown ){
-                console.log( errorThrown );
+            error: function(request, message, error ){
+                handleException(request, message, error);
             }
-        });
 
-        e.preventDefault();
-    }
 
-    $('#my-form').submit( processForm );
-})(jQuery);
+
+            })
+            
+            
+
+        }
+    })(jQuery);
+
+
+
+
+
+
+});
+
