c55d1b6377a7e6d11809d9dc0cd0b96e7634d175
jjvega86 <jj.vega86@gmail.com>
2020-11-17 19:12:51 -0500

    Added PUT method to update movie and form to index.html

--- Client/app.js
+++ Client/app.js
@@ -1,61 +1,77 @@
-$(document).ready(function(){
-    (function($){
-        function processForm( e ){
-            var dict = {
-                Title : this["title"].value,
-                Director: this["director"].value,
-                Genre: this["genre"].value
-            };
-    
-            $.ajax({
-                url: 'https://localhost:44325/api/movie',
-                dataType: 'json',
-                type: 'post',
-                contentType: 'application/json',
-                data: JSON.stringify(dict),
-                success: function( data ){
-                    $('#response pre').html( data );
+(function($){
+    function addMovie( e ){
+        var dict = {
+            Title : this["title"].value,
+            Director: this["director"].value,
+            Genre: this["genre"].value
+        };
 
-                },
-                error: function( errorThrown ){
-                    console.log( errorThrown );
-                }
-            });
-    
-            e.preventDefault();
-        }
-    
-        $('#my-form').submit( processForm);
+        $.ajax({
+            url: 'https://localhost:44325/api/movie',
+            dataType: 'json',
+            type: 'post',
+            contentType: 'application/json',
+            data: JSON.stringify(dict),
+            success: function( data ){
+                $('#response pre').html( data );
 
-    
-        $(function(){
-            $.get("https://localhost:44325/api/movie/", function(data){    
-    
-                data.map(function(el){
-    
-                    
-                    $("#tbody").append(`
-             
-                    <tr>
-                    <td><p>${el.title}</p></td> 
-                    <td><p>${el.director}</p></td>
-                    <td><p>${el.genre}</p></td>
-                    <td><p>DELETE BUTTON</p></td>                 
-                    </tr>`);
-    
-                })
-            })      
+            },
+            error: function( errorThrown ){
+                console.log( errorThrown );
+            }
         });
 
-    
-            
-            
-    
-        
-    })(jQuery);
+        e.preventDefault();
+    }
+
+    $('#my-form').submit(addMovie);
+
+    function updateMovie(e){
+        var dict = {
+            Title : this["title"].value,
+            Director: this["director"].value,
+            Genre: this["genre"].value
+        };
+
+        $.ajax({
+            url: 'https://localhost:44325/api/movie',
+            dataType: 'json',
+            type: 'put',
+            contentType: 'application/json',
+            data: JSON.stringify(dict),
+            success: function( data ){
+                $('#response2 pre').html( data );
+
+            },
+            error: function( errorThrown ){
+                console.log( errorThrown );
+            }
+        });
+
+        e.preventDefault();
+    }
+    $('#my-form2').submit(updateMovie);
+
+
+
+
+    $.get("https://localhost:44325/api/movie/", function(data){    
+
+        data.map(function(el){
+
+            $("#tbody").append(
+            `<tr>
+            <td><p>${el.title}</p></td> 
+            <td><p>${el.director}</p></td>
+            <td><p>${el.genre}</p></td>
+            <td><button re>Update</button></td>                                 
+            </tr>`);
+        })
+    });  
+
+})(jQuery);
    
 
-});
 
 
 
